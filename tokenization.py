cab1b0e3ce496a743c28625e3e0fc0d7420b4ac9
mowi <mollywi@chalmers.se>
2025-09-18 14:58:41 +0200

    Started coding

--- tokenization.py
+++ tokenization.py
@@ -1,2 +1,44 @@
-def tokenize():
-    pass
+def tokenize(txt:str):
+    array = []
+    word = ""
+    print(word)
+    txt = txt.lower()
+    print(txt)
+    for char in txt:
+        if char.isalpha():
+            word += char
+            continue
+
+        if char.isdigit():
+            word += char
+            continue
+        
+            
+        word += char
+        
+        array.append(word)
+        print(array)
+        word = ""
+    return array
+
+tokenize('12 cider is not grey, but it can be almost grey.')
+
+
+
+
+
+#insert(array)
+
+
+
+# def tokenize(lines):
+#     words = []
+#     for line in lines:
+#         start = 0
+#         while start < len(line):
+#             print(line[start])
+#             start = start+1
+#             print(line.split())
+    
+
+# print()" , ".join(input.lower().split(", ").split(". ")).split())
