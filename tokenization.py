f20d8cc01be5b8e350d55315dbd9678e7720ab78
mowi <mollywi@chalmers.se>
2025-09-24 14:38:24 +0200

    Done?
    We deleted some unnecessary code.

--- tokenization.py
+++ tokenization.py
@@ -32,5 +32,3 @@ def tokenize(lst:str):
 
 
     return main_array
-
-print(tokenize(['"They had 16 rolls of duct tape, 2 bags of clothes pins,','130 hampsters from the cancer labs down the hall, and','at least 500 pounds of grape jello and unknown amounts of chopped liver"','said the source on a recent Geraldo interview.']))
